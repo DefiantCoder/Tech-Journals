f74c7dbe97a1202aca2b2df44ab5eff356d79c9b
DefiantCoder <sonoflambchop@gmail.com>
2022-12-05 01:22:53 +0000

    new file

---
+++ linux/centos7/secure-ssh.sh
@@ -0,0 +1,6 @@
+# secure-ssh.sh
+## author adam L
+# creates a new ssh user using $l parameter
+# adds a public key from the local repo or curled from the remote repo
+# removes roots ability to ssh in
+echo "ALL YOUR CODE GOES HERE"
