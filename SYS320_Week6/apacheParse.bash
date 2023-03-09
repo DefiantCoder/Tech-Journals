5a4ea20e1d6e490b989a1a23f5c64c261a838ec4
DefiantCoder <90061680+DefiantCoder@users.noreply.github.com>
2023-03-09 13:06:48 -0500

    Create apacheParse.bash

---
+++ SYS320_Week6/apacheParse.bash
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Function: Parses a log file and formats it
+
+# read in file
+
+#Arguments using the position starting at 1
+
+APACHE_LOG="$1"
+
+#Checking if file exists
+
+if [[ ! -f ${APACHE_LOG} ]]
+then
+	echo "Please specify the path to a log file"
+	exit 1 
+fi
+
+# 81.19.44.12 - - [30/Sep/2018:06:26:56 -0500] "GET / HTTP/1.1" 200 225 "-" "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:57.0) Gecko/20100101 Firefox/57.0"
+
+# Looking for web scanners
+
+#looking for web scanners
+sed -e "s/\[//g" -e "s/\"//g" ${APACHE_LOG} | \
+egrep -i "test|shell|echo|passwd|select|phpmyadmin|setup|admin|w00t" | \
+awk ' BEGIN { format = "%-15s %-20s %-6s %-6s %-5s %s\n" 
+		printf format, "IP", "Date", "Method", "Status", "Size", "URI"
+		printf format, "--", "----", "------", "------", "----", "---"}
+}
+{ printf format, $1, $4, $6, $9, $10, $7 }'
+
+
+
+
+done
